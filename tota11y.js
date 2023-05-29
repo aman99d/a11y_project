d2d037313ffb03071e14453d9588592684ea5176
aman99d <83946565+aman99d@users.noreply.github.com>
2023-05-29 20:30:04 +0530

    Update tota11y.js

--- tota11y.js
+++ tota11y.js
@@ -1,15 +1,5 @@
-﻿/*!
- * tota11y v0.2.0
- * http://khan.github.io/tota11y
- * 
- * Includes Accessibility Developer Tools
- * http://github.com/GoogleChrome/accessibility-developer-tools
- * 
- * Copyright (c) 2019 Khan Academy
- * Released under the MIT license
- * http://github.com/Khan/tota11y/blob/master/LICENSE.txt
- * 
- * Date: 2019-04-22
+/*!
+
  * 
  */!function(n){var r={};function o(e){if(r[e])return r[e].exports;var t=r[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,o),t.l=!0,t.exports}o.m=n,o.c=r,o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(t,e){if(1&e&&(t=o(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)o.d(n,r,function(e){return t[e]}.bind(null,r));return n},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},o.p="",o(o.s=11)}([function(zt,Xt,e){var Kt;
 /*!
@@ -19,11 +9,6 @@
  * Includes Sizzle.js
  * https://sizzlejs.com/
  *
- * Copyright JS Foundation and other contributors
- * Released under the MIT license
- * https://jquery.org/license
- *
- * Date: 2018-01-20T17:24Z
  */
 /*!
  * jQuery JavaScript Library v3.3.1
@@ -32,11 +17,7 @@
  * Includes Sizzle.js
  * https://sizzlejs.com/
  *
- * Copyright JS Foundation and other contributors
- * Released under the MIT license
- * https://jquery.org/license
- *
- * Date: 2018-01-20T17:24Z
+
  */
 ! function(e, t) {
     "use strict";
